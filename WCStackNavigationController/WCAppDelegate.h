7490f76d5a5afcaa29df126e28fc13f124de9275
Wess Cope <wcope@me.com>
2012-05-07 18:30:28 -0400

    Initial Commit

---
+++ WCStackNavigationController/WCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WCAppDelegate.h
+//  WCStackNavigationController
+//
+//  Created by Wess Cope on 5/7/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WCStackNavigationController/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WCStackNavigationController
+//
+//  Created by Wess Cope on 5/7/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WCAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WCAppDelegate class]));
+    }
+}
